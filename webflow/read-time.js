a7c01daff0be09c7e5c18ce5a85eaa45deddac64
Agif <ghifarimohammadrahadian@gmail.com>
2024-04-04 01:19:49 +0700

    Update read-time calculation to include image count

--- webflow/read-time.js
+++ webflow/read-time.js
@@ -3,11 +3,12 @@ const container = document.querySelectorAll('[read-time-container]');
 container.forEach((item) => {
   const content = item.querySelector('[read-time-content]');
   const duration = item.querySelector('[read-time-duration]');
-  const wordsPerMinute = 200;
+  const wordsPerMinute = item.getAttribute('read-time-words-per-minute') || 200;
+  const secondsPerImage = item.getAttribute('read-time-seconds-per-image') || 5;
 
   const text = content.textContent;
   const wordCount = text.split(/\s/g).length;
-  const readTime = Math.ceil(wordCount / wordsPerMinute);
-
+  const imageCount = content.querySelectorAll('img').length;
+  const readTime = Math.ceil(((wordCount / wordsPerMinute) * 60 + (imageCount * secondsPerImage)) / 60);
   duration.textContent = readTime > 1 ? `${readTime} mins` : `${readTime} min`;
 });
